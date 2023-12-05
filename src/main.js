7468515af7013c5e056e11e9921cace9c0334258
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-12-05 11:41:11 +0200

    added linters

--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ import 'virtual:svg-icons-register';
 import '@/plugins';
 
 import '@/js/global-state';
-import '@/js/modal';
-import { setWheelLastStage } from '@/js/wheel';
-import '@/js/sign-up';
-import '@/js/sidebar';
-import '@/js/terms-and-privacy';
-import { openSignUpModal } from '@/js/sign-up';
+// import '@/js/modal';
+// import { setWheelLastStage } from '@/js/wheel';
+// import '@/js/sign-up';
+// import '@/js/sidebar';
+// import '@/js/terms-and-privacy';
+// import { openSignUpModal } from '@/js/sign-up';
 import useViewportSizes from '@/js/use-viewport-sizes';
-import { getFromLS } from '@/js/local-storage';
+// import { getFromLS } from '@/js/local-storage';
 
-const signUpBtnRef = document.querySelector('.js-sign-up-btn');
+// const signUpBtnRef = document.querySelector('.js-sign-up-btn');
 
 useViewportSizes();
 
-const isLastStage = getFromLS('isLastStage');
+// const isLastStage = getFromLS('isLastStage');
 
-// if (isLastStage) {
-//   setWheelLastStage();
-//   openSignUpModal({ isBlocked: true });
-// }
+// // if (isLastStage) {
+// //   setWheelLastStage();
+// //   openSignUpModal({ isBlocked: true });
+// // }
 
-signUpBtnRef.addEventListener('click', openSignUpModal);
+// signUpBtnRef.addEventListener('click', openSignUpModal);
