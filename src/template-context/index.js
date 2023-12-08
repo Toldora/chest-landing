56b9c6bca60e1628f72c66c0e565b5278611445a
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-12-08 13:34:52 +0200

    statistic, fixed overflow

--- src/template-context/index.js
+++ src/template-context/index.js
@@ -1,8 +1,11 @@
 import prizesCtx from './prizes-ctx';
 import socialCtx from './social-ctx';
+import statisticCtx from './statistic-ctx';
 
 export const TEMPLATE_CONTEXT = {
   prizesCtx,
 
   socialCtx,
+
+  statisticCtx,
 };
---
+++ src/template-context/statistic-ctx.js
@@ -0,0 +1,7 @@
+const statisticCtx = {
+  users: '241 512',
+  cases: '154 392',
+  online: '241 512',
+};
+
+export default statisticCtx;
